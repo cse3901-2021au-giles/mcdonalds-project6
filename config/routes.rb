3976aea0f94db9c70f09075d6f7afb69cd3fa1e9
Sichan Yuh <sichan.yuh@gmail.com>
2021-11-14 18:18:32 -0500

    add page specific title for user static pages using layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'static_user_pages/login'
+  root 'application#login'
   get 'static_user_pages/group'
   get 'static_user_pages/project'
   get 'static_user_pages/evaluation'
-
-  root 'application#login'
 end
--- test/controllers/static_user_pages_controller_test.rb
+++ test/controllers/static_user_pages_controller_test.rb
@@ -2,22 +2,26 @@ require 'test_helper'
 
 class StaticUserPagesControllerTest < ActionDispatch::IntegrationTest
   test "get login page" do
-    get static_user_pages_login_url
+    get root_url
     assert_response :success
+    assert_select "title", "Peer Evaluation Tool | Login"
   end
 
   test "get group page" do
     get static_user_pages_group_url
     assert_response :success
+    assert_select "title", "Peer Evaluation Tool | Group"
   end
 
   test "get project" do
     get static_user_pages_project_url
     assert_response :success
+    assert_select "title", "Peer Evaluation Tool | Project"
   end
 
   test "get evaluation" do
     get static_user_pages_evaluation_url
     assert_response :success
+    assert_select "title", "Peer Evaluation Tool | Evaluation"
   end
 end
