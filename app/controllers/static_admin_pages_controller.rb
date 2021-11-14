3a5be8431b45bd004262de5225ec563d953e8599
Sichan Yuh <sichan.yuh@gmail.com>
2021-11-14 18:43:13 -0500

    static admin pages added, set more or less identical to static user pages

---
+++ app/controllers/static_admin_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticAdminPagesController < ApplicationController
+  def login
+  end
+
+  def project
+  end
+
+  def group
+  end
+
+  def members
+  end
+
+  def student
+  end
+
+  def evaluation
+  end
+
+end
--- test/controllers/static_user_pages_controller_test.rb
+++ test/controllers/static_user_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticUserPagesControllerTest < ActionDispatch::IntegrationTest
   test "get login page" do
     get root_url
     assert_response :success
-    assert_select "title", "Peer Evaluation Tool | Login"
+    assert_select "title", "Peer Evaluation Tool | Student View | Login"
   end
 
   test "get group page" do
     get static_user_pages_group_url
     assert_response :success
-    assert_select "title", "Peer Evaluation Tool | Group"
+    assert_select "title", "Peer Evaluation Tool | Student View | Group"
   end
 
   test "get project" do
     get static_user_pages_project_url
     assert_response :success
-    assert_select "title", "Peer Evaluation Tool | Project"
+    assert_select "title", "Peer Evaluation Tool | Student View | Project"
   end
 
   test "get evaluation" do
     get static_user_pages_evaluation_url
     assert_response :success
-    assert_select "title", "Peer Evaluation Tool | Evaluation"
+    assert_select "title", "Peer Evaluation Tool | Student View | Evaluation"
   end
 end
