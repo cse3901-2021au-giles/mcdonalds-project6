b60374c6e421a11db48c7f7e73cfe2b3f79f0337
2001mrajesh <2001mrajesh@gmail.com>
2021-11-28 01:45:41 -0500

    updating models

---
+++ app/models/admin.rb
@@ -0,0 +1,13 @@
+class Admin < ApplicationRecord
+    has_and_belongs_to_many :users
+
+    before_save { a_email.downcase! }
+    validates :a_name, presence: true, length: { maximum: 50 }
+
+    VALID_EMAIL_REGEX = /\A[a-z]+\.[1-9]\d*@osu\.edu/i
+    validates :a_email, presence: true, length: { maximum: 255 },
+                format: { with: VALID_EMAIL_REGEX },
+                uniqueness: true
+
+    validates :a_password, presence: true, length: { minimum: 8 }
+end
