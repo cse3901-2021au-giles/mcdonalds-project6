bcbcbb150fbf94dfdbf803490cae901d230021c3
2001mrajesh <69177582+2001mrajesh@users.noreply.github.com>
2021-12-01 16:29:17 -0500

    update project and group controller methods for creating new

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,11 +5,40 @@ class AdminController < ApplicationController
     @users = User.all
   end
 
-  def create_group
-     @group = group.new
-  end
+  
+   def new_group
+        @group= Group.new
+    end
 
-  def create_proj
-    @project = project.new 
-  end
+    def create_group
+        @group = Group.new(group_params)
+        if @group.save 
+            redirect_to root_path, notice: "Successfully created group"
+        else
+            render :new
+        end
+    end
+
+    def  group_params
+        params.require(:group).permit(:groupname)
+    end
+  
+  
+  def new_proj
+        @proj= Project.new
+    end
+
+    def create_group
+        @proj = Project.new(group_params)
+        if @proj.save 
+            redirect_to root_path, notice: "Successfully created project"
+        else
+            render :new
+        end
+    end
+
+    def  proj_params
+        params.require(:proj).permit(:projectname, :projecttype, :deadline)
+    end
+
 end
