81145a5ca62c844941c1bcfb03681417594f1008
PeilinLi0310 <li.9532>
2021-12-07 18:30:01 -0500

    delete nav bar logo

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,10 +1,14 @@
 require "test_helper"
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  
   test "should get index" do
     get admin_home_path
     assert_response :success
   end
 
+
+ # test "should get create_proj" do
+  #  get admin_create_proj_url
+   # assert_response :success
+  #end
 end
